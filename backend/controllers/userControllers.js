0b9b892503f5c89be8e291094c8dc4b764fd8720
1DeniSetiawan <191240000934@unisnu.ac.id>
2022-07-25 00:02:15 +0700

    protect routes & getMe() DONE

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-
 // @desc    Register user
 // @route   POST /api/users
 // @access  Public
@@ -12,26 +11,21 @@ const registerUser = asyncHandler(async(req, res) => {
         res.status(400);
         throw Error("Please add all fields");
     }
-
     //check if user exist
     const userExists = await User.findOne({ email });
-
     if (userExists) {
         res.status(400);
         throw Error("User already exists");
     }
-
     //hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-
     //create user
     const user = await User.create({
         name,
         email,
         password: hashedPassword,
     });
-
     if (user) {
         res.status(201).json({
             _id: user.id,
@@ -44,16 +38,13 @@ const registerUser = asyncHandler(async(req, res) => {
         throw Error("Invalid user data");
     }
 });
-
 // @desc    Login user
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async(req, res) => {
     const { email, password } = req.body;
-
     //check for user email
     const user = await User.findOne({ email });
-
     if (user && (await bcrypt.compare(password, user.password))) {
         res.json({
             _id: user.id,
@@ -66,12 +57,17 @@ const loginUser = asyncHandler(async(req, res) => {
         throw Error("Invalid credentials");
     }
 });
-
 // @desc    Get user data
 // @route   GET /api/users/me
 // @access  Private
 const getMe = asyncHandler(async(req, res) => {
-    res.json({ message: "User data display" });
+    const { _id, name, email } = await User.findById(req.user.id);
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    });
 });
 
 //generate JWT
@@ -80,7 +76,6 @@ const generateToken = (id) => {
         expiresIn: "30d",
     });
 };
-
 module.exports = {
     registerUser,
     loginUser,
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const {
     registerUser,
     loginUser,
-    getMe
-} = require('../controllers/userController.js')
-router.post('/', registerUser)
-router.post('/login', loginUser)
-router.get('/me', getMe)
+    getMe,
+} = require("../controllers/userController.js");
+const { protect } = require("../middleware/authMiddleware");
 
-module.exports = router
+router.post("/", registerUser);
+router.post("/login", loginUser);
+router.get("/me", protect, getMe);
+
+module.exports = router;
