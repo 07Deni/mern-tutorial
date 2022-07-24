e14b325bae6c9e3e6307f30b494b0569b45a68c5
1DeniSetiawan <191240000934@unisnu.ac.id>
2022-07-24 23:34:59 +0700

    user route & controller (test postman)

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,14 @@ const dotenv = require('dotenv').config()
 const connectDb = require('./config/db')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
-
 connectDb()
-
 const app = express()
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/goals', require('./routes/goalRoutes.js'))
+app.use('/api/users', require('./routes/userRoutes'))
+
 app.use(errorHandler)
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
