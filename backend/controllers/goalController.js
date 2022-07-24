ffc920a583a288edc44e11474ca4b46811f8f742
1DeniSetiawan <191240000934@unisnu.ac.id>
2022-07-24 19:58:33 +0700

    accepting body data

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,13 +2,16 @@
 // @route   GET /api/goals
 // @access  Private 
 const getGoals = (req, res) => {
-    res.status(200).json({ message: 'Get Goals' })
-}
-
-// @desc    Set goals
-// @route   POST /api/goals
-// @access  Private 
+        res.status(200).json({ message: 'Get Goals' })
+    }
+    // @desc    Set goals
+    // @route   POST /api/goals
+    // @access  Private 
 const setGoal = (req, res) => {
+    if (!req.body.text) {
+        res.status(400).json({ message: 'Please add a text field' })
+    }
+
     res.status(200).json({ message: 'Set Goal' })
 }
 
@@ -16,16 +19,14 @@ const setGoal = (req, res) => {
 // @route   PUT /api/goals/:id
 // @access  Private 
 const updateGoal = (req, res) => {
-    res.status(200).json({ message: `Update Goal ${req.params.id}` })
-}
-
-// @desc    Delete goal
-// @route   DELETE /api/goals/:id
-// @access  Private 
+        res.status(200).json({ message: `Update Goal ${req.params.id}` })
+    }
+    // @desc    Delete goal
+    // @route   DELETE /api/goals/:id
+    // @access  Private 
 const deleteGoal = (req, res) => {
     res.status(200).json({ message: `Delete Goal ${req.params.id}` })
 }
-
 module.exports = {
     getGoals,
     setGoal,
