7a6993bc427d7f62846a77ca562c0bf9962eeb90
1DeniSetiawan <191240000934@unisnu.ac.id>
2022-07-24 23:03:33 +0700

    error handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -8,16 +8,16 @@ const getGoals = (req, res) => {
     // @route   POST /api/goals
     // @access  Private 
 const setGoal = (req, res) => {
-    if (!req.body.text) {
-        res.status(400).json({ message: 'Please add a text field' })
+        if (!req.body.text) {
+            res.status(400)
+            throw new Error('Please add a text field')
+        }
+
+        res.status(200).json({ message: 'Set Goal' })
     }
-
-    res.status(200).json({ message: 'Set Goal' })
-}
-
-// @desc    Update goals
-// @route   PUT /api/goals/:id
-// @access  Private 
+    // @desc    Update goals
+    // @route   PUT /api/goals/:id
+    // @access  Private 
 const updateGoal = (req, res) => {
         res.status(200).json({ message: `Update Goal ${req.params.id}` })
     }
