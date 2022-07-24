2e443edb7f83872ba943a502e73e5bf8054687fa
1DeniSetiawan <191240000934@unisnu.ac.id>
2022-07-24 23:31:12 +0700

    add user to goal model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'Please add a name']
+        required: [true, "Please add a name"],
     },
     email: {
         type: String,
-        required: [true, 'Please add an email'],
-        unique: true
+        required: [true, "Please add an email"],
+        unique: true,
     },
     password: {
         type: String,
-        required: [true, 'Please add a password']
+        required: [true, "Please add a password"],
     },
 }, {
-    timestamps: true
-})
+    timestamps: true,
+});
 
-module.exports = mongoose.model('Users', userSchema)
+module.exports = mongoose.model("Users", userSchema);
